e4b7b3ab0b635649e933a77e5cd344eaeea0620f
Tanvir <toukir.developer.bd@gmail.com>
2023-06-10 02:04:00 +0600

    post and get users

--- index.js
+++ index.js
@@ -27,26 +27,43 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const selectCoursesCollection = client.db("summer-camp").collection("selectCourses");
         const coursesCollection = client.db("summer-camp").collection("courses");
         const paymentsCollection = client.db("summer-camp").collection("payments");
+        const usersCollection = client.db("summer-camp").collection("users");
 
         // create payment intent 
         app.post('/create-payment-intent', async (req, res) => {
+            try {
+                const { price } = req.body || {}
+                const amount = Math.round(price * 100);
+                // console.log(price, amount);
+                const paymentIntent = await stripe.paymentIntents.create({
+                    amount: amount,
+                    currency: 'usd',
+                    payment_method_types: [
+                        "card"
+                    ]
+                })
+                res.send({
+                    clientSecret: paymentIntent.client_secret,
+                })
+            }
+            catch (error) {
+                res.send({ error: error.message })
+            }
+        })
 
-            const { price } = req.body || {}
-            const amount = Math.round(price * 100);
-            console.log(price, amount);
-            const paymentIntent = await stripe.paymentIntents.create({
-                amount: amount,
-                currency: 'usd',
-                payment_method_types: [
-                    "card"
-                ]
-            })
+        // users apis
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray()
+            res.send(result)
+        })
 
-            res.send({
-                clientSecret: paymentIntent.client_secret,
-            })
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
         })
 
         // payment related api
@@ -58,29 +75,56 @@ async function run() {
 
         app.post("/payments", async (req, res) => {
             const payment = req.body;
-            console.log(payment);
+            console.log(payment.courses_id);
             const insertResult = await paymentsCollection.insertOne(payment);
             const query = { _id: { $in: payment.selected_courses_id.map(id => new ObjectId(id)) } }
-            const deletedResult = await coursesCollection.deleteMany(query)
+            const deletedResult = await selectCoursesCollection.deleteMany(query)
+
+
+            await payment.courses_id.map(async (singleId) => {
+                const { available_seat } = await coursesCollection.findOne({ course_id: singleId })
+                const updateDoc = {
+                    $set: {
+                        available_seat: available_seat - 1
+                    },
+                };
+                const newData = await coursesCollection.updateOne({ course_id: singleId }, updateDoc)
+                console.log(newData);
+            })
+
+
+
             res.send({ insertResult, deletedResult })
         })
 
-        // classes api
+        // courses api 
         app.get('/courses', async (req, res) => {
             const result = await coursesCollection.find().toArray()
             res.send(result)
         })
 
-        app.post("/courses", async (req, res) => {
-            const course = req.body;
-            const result = await coursesCollection.insertOne(course)
+
+
+        // select course api
+        app.get('/selectCourses', async (req, res) => {
+            const result = await selectCoursesCollection.find().toArray()
             res.send(result)
         })
 
-        app.delete("/courses/:id", async (req, res) => {
+        app.post("/selectCourses", async (req, res) => {
+            const course = req.body;
+            const result = await selectCoursesCollection.insertOne(course)
+
+            res.send(result)
+        })
+
+        app.delete("/selectCourses/:id", async (req, res) => {
             const id = req.params.id;
+            // console.log(id);
             const query = { _id: new ObjectId(id) }
-            const result = await coursesCollection.deleteOne(query)
+            // console.log(query);
+            const result = await selectCoursesCollection.deleteOne(query)
+
             res.send(result)
 
         })
